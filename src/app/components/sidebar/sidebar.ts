267a3c67365fc7d0289eea0bcf92dcd361cba91d
Kaushal Brahmbhatt <kaushalbrahmbhatt38@gmail.com>
2026-07-03 03:15:38 +0530

    Updated Navigation bar and Header section

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
+import { ScrollService } from '../../service/scroll/scroll-service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,4 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  isScrolled = false;
+  menuOpen = false;
+  private scrollService:ScrollService;
+
+  constructor() {
+    this.scrollService = inject(ScrollService);
+  }
+
+  ngOnInit(): void {
+    this.isScrolled = window.scrollY > 50;
+  }
+
+  @HostListener('window:scroll')
+  onScroll(): void {
+    this.isScrolled = window.scrollY > 50;
+  }
+
+  scrollTo(elementId: string): void {
+    this.menuOpen = false;
+    this.scrollService.scrollToElement(elementId);
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+}
