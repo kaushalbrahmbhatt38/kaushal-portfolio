23ff948af12730345f951dc04d163dcb1abb657d
Kaushal Brahmbhatt <kaushalbrahmbhatt38@gmail.com>
2026-07-03 02:46:06 +0530

    created basic project folder structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Header } from './components/header/header';
+import { Experience } from './components/experience/experience';
+import { About } from './components/about/about';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Education } from './components/education/education';
+import { Contact } from './components/contact/contact';
+import { BackToTop } from './components/back-to-top/back-to-top';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Sidebar,
+    Header,
+    Experience,
+    About,
+    Portfolio,
+    Education,
+    Contact,
+    BackToTop
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Kaushal-portfolio');
+  protected readonly title = signal('Kaushal Brahmbhatt Portfolio');
+  currentYear = new Date().getFullYear();
 }
---
+++ src/app/components/back-to-top/back-to-top.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-to-top',
+  imports: [],
+  templateUrl: './back-to-top.html',
+  styleUrl: './back-to-top.css',
+})
+export class BackToTop {}
