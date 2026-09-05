967f11fdfb7a7ddf9424272ed63a949dbc32f461
Kaushal Brahmbhatt <kaushalbrahmbhatt38@gmail.com>
2026-09-05 20:03:50 +0530

    integrated contact form with backend to store contact form data

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -19,18 +19,18 @@ export class Contact {
 
   ngOnInit(): void {
     this.contactForm = this._formBuilder.group({
-      'oid': ['00D5j00000DqLwg'],
-      'retURL': ["https://kaushal-portfolio-ejj078hvs-kaushal-brahmbhatt.vercel.app"],
+      'sheetName': ['ContactForm'],
       'first_name': ['', [Validators.required]],
       'last_name': ['', [Validators.required]],
       'email': ['', [Validators.required, Validators.email]],
       'mobile': ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern("[0-9]{10}")]],
-      '00N5j00000TrnOR': [''],
+      'job_profile': [''],
       'company': ['', [Validators.required]],
       'url': [''],
       'city': [''],
       'state': [''],
-      'description': ['', Validators.maxLength(32000)]
+      'description': ['', Validators.maxLength(32000)],
+      'date':[]
     });
   }
 
@@ -39,17 +39,31 @@ export class Contact {
     console.log('Your order has been submitted', JSON.stringify(this.contactForm.value));
 
     if (this.contactForm.valid) {
-      let url = "https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8&orgId=00D5j00000DqLwg";
-      let httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/x-www-form-urlencoded'
-        })
-      };
+
+      this.contactForm.patchValue({sheetName: 'ContactForm', date: new Date().toISOString()});
+
+      const contactFormData = new FormData();
+
+      Object.keys(this.contactForm.value).forEach(key => {
+        contactFormData.append(key, this.contactForm.value[key]);
+      });
+
+      let url = "https://script.google.com/macros/s/AKfycbw0oZXtBazfRs3NHgmbzj93hYIFg5FEKgurUt6kYhiCzBxwwf4pUiwes5ZnIYTNzW0b/exec";
+      
+      const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'text/plain'
+      })
+    };
+
       this._http.post(url, JSON.stringify(this.contactForm.value), httpOptions).subscribe({
-        next: () => { console.log('sucess') },
+        next: () => { 
+          console.log('sucess');
+          this.contactForm.reset();
+        },
         error: (err: any) => { console.log('error', err) }
       })
-      this.contactForm.reset();
+      
     }
   }
 }
